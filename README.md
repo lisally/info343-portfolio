328e52c28b9946577a5f1a22f9ee5619794a3c0d
Sally Li <lisally@uw.edu>
2015-11-22 23:13:19 -0800

    added contact page, styling, and now complete

---
+++ README.md
@@ -0,0 +1,13 @@
+# personal-portfolio
+
+##### 1. What is the URL of this project on your student web-server? #####
+> http://students.washington.edu/lisally/info343/personal-portfolio
+
+##### 2. Did you receive help from any other sources (classmates, etc.)? If so, please list who. #####
+> Angel (Yutong)
+
+##### 3. Approximately how many hours did it take you to complete this challenge? #####
+> 12 hrs
+
+##### 4. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
+> 
--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-$(document).ready(function(){
-    $.ajaxSetup({
-        cache: false
-    });
-});
-
 // Create app
-var myApp = angular.module('myApp', ['ui.router'])
+var myApp = angular.module('myApp', ['ui.router', 'firebase'])
 
 // Configure app
 myApp.config(function($stateProvider) {
     // Config route provider
     $stateProvider
+
+    // Config home page url, template url, and controller
     .state('home', {
         url:'',
         templateUrl: 'templates/home.html',
         controller: 'HomeController',
     })
 
+    // Config dawg-coffee page url, template url, and controller
     .state('dawg-coffee', {
         url:'/dawg-coffee',
         templateUrl: 'templates/dawg-coffee.html',
         controller: 'DCController',
     })
 
+    // Config police-shooting page url, template url, and controller
     .state('police-shooting', {
         url: '/police-shooting',
         templateUrl: 'templates/police-shooting.html',
@@ -30,6 +28,7 @@ myApp.config(function($stateProvider) {
 
     })
 
+    // Config crud-review page url, template url, and controller
     .state('crud-review', {
         url: '/crud-review',
         templateUrl: 'templates/crud-review.html',
@@ -37,28 +36,41 @@ myApp.config(function($stateProvider) {
 
     })
 
+    // Config spotify-challenge page url, template url, and controller
     .state('spotify-challenge', {
         url: '/spotify-challenge',
         templateUrl: 'templates/spotify-challenge.html',
         controller: 'SCController',
 
     })
+
+    // Config contact page url, template url, and controller
+    .state('contact', {
+        url: '/contact',
+        templateUrl: 'templates/contact.html',
+        controller: 'ContactController',
+    })
 })
 
+    // Controller for home page
     .controller('HomeController', function($scope, $http, $timeout) {
+        // Gets data from data.json file for page title and description
         $http.get('data/data.json').success(function(response){
             $scope.data = response[0]
             console.log($scope.data)
             $scope.homeTitle = $scope.data.Title
             $scope.homeDescription = $scope.data.Description
         })
+        // Gets data from data.json2 file for page table information
         $http.get('data/data2.json').then(function(response2) {
             $scope.data2 = response2.data
 
         })    
     })
 
+    // Controller for dawg-coffee page
     .controller('DCController', function($scope, $http, $sce) {
+        // Gets data from data.json file for page title, description, and iframe source
         $http.get('data/data.json', {cache: true}).success(function(response){
             $scope.data = response[1]
             console.log($scope.data)
@@ -68,7 +80,9 @@ myApp.config(function($stateProvider) {
         })
     })
 
+    // Controller for police-shooting page
     .controller('PSController', function($scope, $http, $sce) {
+        // Gets data from data.json file for page title, description, and iframe source
         $http.get('data/data.json').then(function(response){
             $scope.data = response.data[2]
             console.log($scope.data)
@@ -78,7 +92,9 @@ myApp.config(function($stateProvider) {
         })
     })
 
+    // Controller for crud-review page
     .controller('CRController', function($scope, $http, $sce) {
+        // Gets data from data.json file for page title, description, and iframe source
         $http.get('data/data.json').then(function(response){
             $scope.data = response.data[3]
             console.log($scope.data)
@@ -88,7 +104,9 @@ myApp.config(function($stateProvider) {
         })
     })
 
+    // Controller for spotify-challenge page
     .controller('SCController', function($scope, $http, $sce) {
+        // Gets data from data.json file for page title, description, and iframe source
         $http.get('data/data.json').then(function(response){
             $scope.data = response.data[4]
             console.log($scope.data)
@@ -98,3 +116,33 @@ myApp.config(function($stateProvider) {
 
         })
     })
+
+    // Controller for contact page
+    .controller('ContactController', function($scope, $firebaseArray) {
+        // Creates new firebase reference
+        var ref = new Firebase("https://lisallycontactapp.firebaseio.com/");
+        var contactsRef = ref.child('contacts')
+
+        // Creates firebase array for contact messages
+        $scope.contacts = $firebaseArray(contactsRef)
+
+        // Function to submit name, email, and message to firebase applcation with timestamp
+        $scope.submit = function() {
+            $scope.contacts.$add({
+                name: $scope.name,
+                email: $scope.email,
+                message: $scope.message,
+                time: Firebase.ServerValue.TIMESTAMP
+            })
+        
+        // Clears the name, email, and message input boxes and saves messages to firebase application
+            .then(function() {
+                $scope.name = "";
+                $scope.email = "";
+                $scope.message = "";
+                $scope.contacts.$save();
+                alert("Thanks for leaving a message, Sally will contact you back as soon as possible!");
+            })
+        }
+    })    
+
